8bde5a4ca545fab9aaacf96aac0f9fbab2f380c2
yxy <yan1075783878@126.com>
2026-05-20 15:56:54 +0800

    remove ros build artifacts

--- patchwork-plusplus-ros/build/patchworkpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/unilidar_sdk2/patchwork-plusplus-ros
-# Build directory: /home/ubuntu/unilidar_sdk2/patchwork-plusplus-ros/build/patchworkpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
