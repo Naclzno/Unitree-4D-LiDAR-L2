831a607836712edb61e58c7230bdc0074165e700
yxy <yan1075783878@126.com>
2026-05-20 15:53:55 +0800

    remove ros build artifacts

--- point_lio_unilidar-2.0.2/build/point_lio_unilidar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/unilidar_sdk2/point_lio_unilidar-2.0.2
-# Build directory: /home/ubuntu/unilidar_sdk2/point_lio_unilidar-2.0.2/build/point_lio_unilidar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
